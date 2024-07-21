72fc42db2d4821000d80fc888a36b372a28afc9a
developern1 <developern.con@gmail.com>
2024-07-21 16:46:31 +0900

    Edit FooterBar styles

--- src/components/FooterBar.jsx
+++ src/components/FooterBar.jsx
@@ -1,11 +1,24 @@
-
-
 export default function FooterBar() {
   return (
-    <>
-      <a href="https://naver.com" target="_blank">
-        사용설명서(준비중!)
-      </a>
-    </>
+    <div style={styles.footer}>
+      스타일 적용
+    </div>
   )
 }
+
+const styles = {
+  footer: {
+    // position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    height: '60px',
+    backgroundColor: '#f5f5f5',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footerText: {
+    color: '#333',
+    fontSize: '1.2rem',
+  },
+};
